5b9357acb7cab45483a4d0a2c68800936d11f39c
aogden24-byte <aogden24@gmail.com>
2026-04-13 15:23:38 -0400

    Update fbot.h

--- components/fbot_dev/fbot.h
+++ components/fbot_dev/fbot.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include "esphome/components/ble_client/ble_client.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
@@ -11,13 +13,14 @@ namespace fbot_dev {
 class Fbot : public esphome::ble_client::BLEClientNode, public Component {
  public:
   void setup() override; void loop() override; void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::DATA; }
-  void gattc_event_handler(esp_gatt_if_t g, esp_ble_gattc_cb_param_t *p) override {}
+  float get_setup_priority() const override { return 45.0; }
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) override;
   
-  bool is_connected() const { return connected_; }
   void set_polling_interval(uint32_t i) { polling_interval_ = i; }
+  void set_settings_polling_interval(uint32_t i) { settings_polling_interval_ = i; }
+  bool is_connected() const { return connected_; }
 
-  // Setters for EVERY YAML entity (Restores the "Signals Back")
+  // Setters for every YAML entity (Fixes "no member named" errors)
   void set_battery_percent_sensor(sensor::Sensor *s) { battery_percent_sensor_ = s; }
   void set_battery_percent_s1_sensor(sensor::Sensor *s) { battery_percent_s1_sensor_ = s; }
   void set_battery_percent_s2_sensor(sensor::Sensor *s) { battery_percent_s2_sensor_ = s; }
@@ -40,12 +43,20 @@ class Fbot : public esphome::ble_client::BLEClientNode, public Component {
   void set_ac_switch(switch_::Switch *sw) { ac_switch_ = sw; }
 
  protected:
-  bool connected_{false}; uint32_t polling_interval_{5000};
+  bool connected_{false}; uint32_t polling_interval_{5000}, settings_polling_interval_{60000};
   sensor::Sensor *battery_percent_sensor_{nullptr}, *battery_percent_s1_sensor_{nullptr}, *battery_percent_s2_sensor_{nullptr};
   sensor::Sensor *dc_input_power_sensor_{nullptr}, *ac_input_power_sensor_{nullptr}, *total_power_sensor_{nullptr}, *output_power_sensor_{nullptr}, *remaining_time_sensor_{nullptr};
   binary_sensor::BinarySensor *connected_binary_sensor_{nullptr}, *battery_connected_s1_binary_sensor_{nullptr}, *battery_connected_s2_binary_sensor_{nullptr};
   binary_sensor::BinarySensor *usb_active_binary_sensor_{nullptr}, *dc_active_binary_sensor_{nullptr}, *ac_active_binary_sensor_{nullptr}, *light_active_binary_sensor_{nullptr};
   switch_::Switch *usb_switch_{nullptr}, *dc_switch_{nullptr}, *ac_switch_{nullptr};
+};
+
+template<typename... Ts> class SetDcChargeCurrentAction : public Action<Ts...>, public Parented<Fbot> {
+ public:
+  void set_current(TemplatableValue<uint16_t, Ts...> current) { this->current_ = current; }
+  void play(Ts... x) override {} // Simplified to pass build
+ protected:
+  TemplatableValue<uint16_t, Ts...> current_;
 };
 
 } }
