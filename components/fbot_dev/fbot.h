b798adba35f03f09e580815c4b1b985dcab4df03
aogden24-byte <aogden24@gmail.com>
2026-04-13 15:17:33 -0400

    Update fbot.h

--- components/fbot_dev/fbot.h
+++ components/fbot_dev/fbot.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "esphome/components/ble_client/ble_client.h"
@@ -6,8 +7,13 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/switch/switch.h"
+
+#ifdef USE_NUMBER
 #include "esphome/components/number/number.h"
+#endif
+#ifdef USE_SELECT
 #include "esphome/components/select/select.h"
+#endif
 
 #ifdef USE_ESP32
 #include <esp_gattc_api.h>
@@ -19,7 +25,6 @@ static const char *const SERVICE_UUID = "0000a002-0000-1000-8000-00805f9b34fb";
 static const char *const WRITE_CHAR_UUID = "0000c304-0000-1000-8000-00805f9b34fb";
 static const char *const NOTIFY_CHAR_UUID = "0000c305-0000-1000-8000-00805f9b34fb";
 
-// All hardware registers for Fossi/Aferiy units
 static const uint8_t REG_USB_CONTROL = 24;
 static const uint8_t REG_DC_CONTROL = 25;
 static const uint8_t REG_AC_CONTROL = 26;
@@ -41,7 +46,7 @@ class Fbot : public esphome::ble_client::BLEClientNode, public Component {
   void set_polling_interval(uint32_t interval) { this->polling_interval_ = interval; }
   void set_settings_polling_interval(uint32_t interval) { this->settings_polling_interval_ = interval; }
   
-  // Public Getters and Control Methods (Required for Switches/Numbers/Selects)
+  // Public Interface for Switches and Numbers
   bool is_connected() const { return connected_; }
   void control_usb(bool state);
   void control_dc(bool state);
@@ -49,21 +54,38 @@ class Fbot : public esphome::ble_client::BLEClientNode, public Component {
   void control_light(bool state);
   void control_ac_silent(bool state);
   void control_key_sound(bool state);
-  void control_light_mode(const std::string &value);
   void set_threshold_charge(float percent);
   void set_threshold_discharge(float percent);
   void set_dc_charge_current(uint16_t current);
 
-  // All Sensor Setters for fbot_dev
+  // Sensor Setters
   void set_battery_percent_sensor(sensor::Sensor *s) { battery_percent_sensor_ = s; }
   void set_battery_percent_s1_sensor(sensor::Sensor *s) { battery_percent_s1_sensor_ = s; }
   void set_battery_percent_s2_sensor(sensor::Sensor *s) { battery_percent_s2_sensor_ = s; }
   void set_dc_input_power_sensor(sensor::Sensor *s) { dc_input_power_sensor_ = s; }
   void set_ac_input_power_sensor(sensor::Sensor *s) { ac_input_power_sensor_ = s; }
-  void set_output_power_sensor(sensor::Sensor *s) { output_power_sensor_ = s; }
   void set_total_power_sensor(sensor::Sensor *s) { total_power_sensor_ = s; }
   void set_remaining_time_sensor(sensor::Sensor *s) { remaining_time_sensor_ = s; }
+  void set_output_power_sensor(sensor::Sensor *s) { output_power_sensor_ = s; }
+
+  // Binary Sensor Setters (Fixed missing members)
   void set_connected_binary_sensor(binary_sensor::BinarySensor *s) { connected_binary_sensor_ = s; }
+  void set_battery_connected_s1_binary_sensor(binary_sensor::BinarySensor *s) { battery_connected_s1_binary_sensor_ = s; }
+  void set_battery_connected_s2_binary_sensor(binary_sensor::BinarySensor *s) { battery_connected_s2_binary_sensor_ = s; }
+  void set_usb_active_binary_sensor(binary_sensor::BinarySensor *s) { usb_active_binary_sensor_ = s; }
+  void set_dc_active_binary_sensor(binary_sensor::BinarySensor *s) { dc_active_binary_sensor_ = s; }
+  void set_ac_active_binary_sensor(binary_sensor::BinarySensor *s) { ac_active_binary_sensor_ = s; }
+  void set_light_active_binary_sensor(binary_sensor::BinarySensor *s) { light_active_binary_sensor_ = s; }
+
+  // Switch/Number Setters (Fixed missing members)
+  void set_usb_switch(switch_::Switch *sw) { usb_switch_ = sw; }
+  void set_dc_switch(switch_::Switch *sw) { dc_switch_ = sw; }
+  void set_ac_switch(switch_::Switch *sw) { ac_switch_ = sw; }
+  void set_light_switch(switch_::Switch *sw) { light_switch_ = sw; }
+  void set_ac_silent_switch(switch_::Switch *sw) { ac_silent_switch_ = sw; }
+  void set_key_sound_switch(switch_::Switch *sw) { key_sound_switch_ = sw; }
+  void set_threshold_charge_number(number::Number *n) { threshold_charge_number_ = n; }
+  void set_threshold_discharge_number(number::Number *n) { threshold_discharge_number_ = n; }
 
   void send_control_command(uint16_t reg, uint16_t value);
 
@@ -75,23 +97,26 @@ class Fbot : public esphome::ble_client::BLEClientNode, public Component {
   sensor::Sensor *battery_percent_sensor_{nullptr}, *battery_percent_s1_sensor_{nullptr}, *battery_percent_s2_sensor_{nullptr};
   sensor::Sensor *dc_input_power_sensor_{nullptr}, *ac_input_power_sensor_{nullptr}, *total_power_sensor_{nullptr};
   sensor::Sensor *remaining_time_sensor_{nullptr}, *output_power_sensor_{nullptr};
-  binary_sensor::BinarySensor *connected_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *connected_binary_sensor_{nullptr}, *battery_connected_s1_binary_sensor_{nullptr}, *battery_connected_s2_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *usb_active_binary_sensor_{nullptr}, *dc_active_binary_sensor_{nullptr}, *ac_active_binary_sensor_{nullptr}, *light_active_binary_sensor_{nullptr};
+  switch_::Switch *usb_switch_{nullptr}, *dc_switch_{nullptr}, *ac_switch_{nullptr}, *light_switch_{nullptr}, *ac_silent_switch_{nullptr}, *key_sound_switch_{nullptr};
+  number::Number *threshold_charge_number_{nullptr}, *threshold_discharge_number_{nullptr};
 
   uint16_t calculate_checksum(const uint8_t *data, size_t len);
   void generate_command_bytes(uint8_t addr, uint16_t reg, uint16_t val, uint8_t *out);
   void send_read_request();
-  void parse_notification(const uint8_t *data, uint16_t len);
   void update_connected_state(bool state);
 };
 
+// FIXED: Correct Action Bridge signature for TemplatableValue
 template<typename... Ts> class SetDcChargeCurrentAction : public Action<Ts...>, public Parented<Fbot> {
  public:
-  void set_current(TemplatableValue<uint16_t> current) { this->current = current; }
-  void play(const Ts &...x) override {
-    this->parent_->set_dc_charge_current(this->current.value(x...));
+  void set_current(TemplatableValue<uint16_t, Ts...> current) { this->current_ = current; }
+  void play(Ts... x) override {
+    this->parent_->set_dc_charge_current(this->current_.value(x...));
   }
  protected:
-  TemplatableValue<uint16_t> current;
+  TemplatableValue<uint16_t, Ts...> current_;
 };
 
 } }
