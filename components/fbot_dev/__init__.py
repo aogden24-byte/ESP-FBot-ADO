9fa68f35f401bc1259bf248ee5764cb8a1703b62
aogden24-byte <aogden24@gmail.com>
2026-04-13 15:02:25 -0400

    Update __init__.py

--- components/fbot_dev/__init__.py
+++ components/fbot_dev/__init__.py
@@ -1,19 +1,41 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
 from esphome.components import ble_client
 from esphome.const import CONF_ID
 
-# Define the namespace for the C++ code
-fbot_dev_ns = cg.esphome_ns.namespace('fbot_dev')
-FBot = fbot_dev_ns.class_('FBot', cg.Component, ble_client.BLEClientNode)
+AUTO_LOAD = ["ble_client"]
+DEPENDENCIES = ["ble_client"]
+MULTI_CONF = True
+
+fbot_ns = cg.esphome_ns.namespace("fbot_dev")
+Fbot = fbot_ns.class_("Fbot", cg.Component, ble_client.BLEClientNode)
+SetDcChargeCurrentAction = fbot_ns.class_("SetDcChargeCurrentAction", automation.Action)
 
 CONFIG_SCHEMA = cv.All(
     cv.Schema({
-        cv.GenerateID(): cv.declare_id(FBot),
-    }).extend(ble_client.BLE_CLIENT_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+        cv.GenerateID(): cv.declare_id(Fbot),
+        cv.Optional("polling_interval", default="2s"): cv.positive_time_period_milliseconds,
+        cv.Optional("settings_polling_interval", default="60s"): cv.positive_time_period_milliseconds,
+    }).extend(cv.COMPONENT_SCHEMA).extend(ble_client.BLE_CLIENT_SCHEMA),
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await ble_client.register_ble_node(var, config)
+
+@automation.register_action(
+    "fbot_dev.set_dc_charge_current",
+    SetDcChargeCurrentAction,
+    automation.maybe_simple_id({
+        cv.GenerateID(): cv.use_id(Fbot),
+        cv.Required("current"): cv.templatable(cv.uint16_t),
+    }),
+)
+async def fbot_set_dc_charge_current_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    template_ = await cg.templatable(config["current"], args, cg.uint16)
+    cg.add(var.set_current(template_))
+    return var
