34a8421781e6e02a72b3a4b4e038a57c68061a26
aogden24-byte <aogden24@gmail.com>
2026-04-13 15:12:35 -0400

    Update fbot.h

--- components/fbot_dev/fbot.h
+++ components/fbot_dev/fbot.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "esphome/components/ble_client/ble_client.h"
@@ -7,10 +6,8 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/switch/switch.h"
-
-#ifdef USE_NUMBER
 #include "esphome/components/number/number.h"
-#endif
+#include "esphome/components/select/select.h"
 
 #ifdef USE_ESP32
 #include <esp_gattc_api.h>
@@ -22,7 +19,16 @@ static const char *const SERVICE_UUID = "0000a002-0000-1000-8000-00805f9b34fb";
 static const char *const WRITE_CHAR_UUID = "0000c304-0000-1000-8000-00805f9b34fb";
 static const char *const NOTIFY_CHAR_UUID = "0000c305-0000-1000-8000-00805f9b34fb";
 
+// All hardware registers for Fossi/Aferiy units
+static const uint8_t REG_USB_CONTROL = 24;
+static const uint8_t REG_DC_CONTROL = 25;
+static const uint8_t REG_AC_CONTROL = 26;
+static const uint8_t REG_LIGHT_CONTROL = 27;
 static const uint8_t REG_DC_CHARGE_CURRENT = 40; 
+static const uint8_t REG_KEY_SOUND = 56;
+static const uint8_t REG_AC_SILENT_CONTROL = 57;
+static const uint8_t REG_THRESHOLD_DISCHARGE = 66;
+static const uint8_t REG_THRESHOLD_CHARGE = 67;
 
 class Fbot : public esphome::ble_client::BLEClientNode, public Component {
  public:
@@ -35,32 +41,52 @@ class Fbot : public esphome::ble_client::BLEClientNode, public Component {
   void set_polling_interval(uint32_t interval) { this->polling_interval_ = interval; }
   void set_settings_polling_interval(uint32_t interval) { this->settings_polling_interval_ = interval; }
   
+  // Public Getters and Control Methods (Required for Switches/Numbers/Selects)
+  bool is_connected() const { return connected_; }
+  void control_usb(bool state);
+  void control_dc(bool state);
+  void control_ac(bool state);
+  void control_light(bool state);
+  void control_ac_silent(bool state);
+  void control_key_sound(bool state);
+  void control_light_mode(const std::string &value);
+  void set_threshold_charge(float percent);
+  void set_threshold_discharge(float percent);
+  void set_dc_charge_current(uint16_t current);
+
+  // All Sensor Setters for fbot_dev
   void set_battery_percent_sensor(sensor::Sensor *s) { battery_percent_sensor_ = s; }
+  void set_battery_percent_s1_sensor(sensor::Sensor *s) { battery_percent_s1_sensor_ = s; }
+  void set_battery_percent_s2_sensor(sensor::Sensor *s) { battery_percent_s2_sensor_ = s; }
   void set_dc_input_power_sensor(sensor::Sensor *s) { dc_input_power_sensor_ = s; }
+  void set_ac_input_power_sensor(sensor::Sensor *s) { ac_input_power_sensor_ = s; }
+  void set_output_power_sensor(sensor::Sensor *s) { output_power_sensor_ = s; }
   void set_total_power_sensor(sensor::Sensor *s) { total_power_sensor_ = s; }
+  void set_remaining_time_sensor(sensor::Sensor *s) { remaining_time_sensor_ = s; }
   void set_connected_binary_sensor(binary_sensor::BinarySensor *s) { connected_binary_sensor_ = s; }
 
-  void set_dc_charge_current(uint16_t current);
   void send_control_command(uint16_t reg, uint16_t value);
 
  protected:
   uint16_t write_handle_{0}, notify_handle_{0};
   uint32_t polling_interval_{2000}, settings_polling_interval_{60000}, last_poll_time_{0};
   bool connected_{false}, characteristics_discovered_{false};
-  sensor::Sensor *battery_percent_sensor_{nullptr}, *dc_input_power_sensor_{nullptr}, *total_power_sensor_{nullptr};
+  
+  sensor::Sensor *battery_percent_sensor_{nullptr}, *battery_percent_s1_sensor_{nullptr}, *battery_percent_s2_sensor_{nullptr};
+  sensor::Sensor *dc_input_power_sensor_{nullptr}, *ac_input_power_sensor_{nullptr}, *total_power_sensor_{nullptr};
+  sensor::Sensor *remaining_time_sensor_{nullptr}, *output_power_sensor_{nullptr};
   binary_sensor::BinarySensor *connected_binary_sensor_{nullptr};
 
   uint16_t calculate_checksum(const uint8_t *data, size_t len);
   void generate_command_bytes(uint8_t addr, uint16_t reg, uint16_t val, uint8_t *out);
   void send_read_request();
+  void parse_notification(const uint8_t *data, uint16_t len);
   void update_connected_state(bool state);
 };
 
 template<typename... Ts> class SetDcChargeCurrentAction : public Action<Ts...>, public Parented<Fbot> {
  public:
-  // The Fix: Added set_current setter for Python and defined 'current' explicitly
   void set_current(TemplatableValue<uint16_t> current) { this->current = current; }
-
   void play(const Ts &...x) override {
     this->parent_->set_dc_charge_current(this->current.value(x...));
   }
